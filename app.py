537af753d1ad0764a6561dd61a4459fde3005834
luizzamprogno <lpzampronio@gmail.com>
2024-08-14 18:26:40 -0300

    Consertamos a lógica das funções update_data e delete_data

--- app.py
+++ app.py
@@ -14,11 +14,8 @@ def create_table(cursor, conexao):
     cursor.execute('''
 
         CREATE TABLE IF NOT EXISTS Tarefas (
-
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-
         tarefa TEXT NOT NULL,
-
         status TEXT NOT NULL
 
         );
@@ -28,57 +25,50 @@ def create_table(cursor, conexao):
 
 def create_data(cursor, conexao):
 
-    tarefa = input('Digite a tarefa: ')
-
-    status = input('Digite o status: ')
+    tarefa = input('Digite a nova tarefa: ')
+    status = input('Digite o status da nova tarefa: ')
 
     cursor.execute('''
 
         INSERT INTO tarefas (tarefa, status)
-
         VALUES (?, ?)
 
     ''', (tarefa, status))
 
-
     conexao.commit()
+    print(f'Tarefa criada com sucesso: {tarefa}')
 
 def read_data(cursor, conexao):
 
     cursor.execute('''
 
         SELECT * FROM tarefas
+
     ''')
 
-
     for linha in cursor.fetchall():
-
         print(linha)
 
 def update_data(cursor, conexao):
 
+    tarefa_a_alterar = int(input('Qual tarefa gostaria de alterar (ID)? '))
     nova_tarefa = input('Digite a nova tarefa: ')
-
     novo_status = input('Digite o novo status: ')
-
 
     cursor.execute('''
 
         UPDATE tarefas
-
         SET tarefa = ?, status = ?
+        WHERE id = ?
 
-        WHERE id = 1
-
-    ''', (nova_tarefa, novo_status))
-
+    ''', (nova_tarefa, novo_status, tarefa_a_alterar))
 
     conexao.commit()
+    print(f'Tarefa de ID {tarefa_a_alterar} atualizada com sucesso')
 
 def delete_data(cursor, conexao):
 
-    deletar_id = input('Qual tarefa deseja deletar: ')
-
+    deletar_id = input('Qual tarefa deseja deletar (ID): ')
 
     cursor.execute('''
 
@@ -86,10 +76,10 @@ def delete_data(cursor, conexao):
 
         WHERE id = ?
 
-    ''', (deletar_id))
-
+    ''', (deletar_id,)) # a vírgula forma uma tupla de um unico elemento
 
     conexao.commit()
+    print(f'Tarefa com id {deletar_id} apagada com sucesso')
 
 def main_menu():
     while True:
@@ -115,11 +105,10 @@ def secondary_menu():
         try:
             return int(input('''O que deseja fazer agora?
                                 Voltar ao meun principal - (1)
-                                Criar outra tarefa - (2)
-                                Sair - (3)
+                                Executar a mesma tarefa - (2)
                         '''))
         except ValueError:
-            print('Entrada inválida, por favor digite um número entre 1 e 5')
+            print('Entrada inválida, por favor digite 1 para voltar ao menu principal, ou 2 para realizar a mesma tarefa')
 
 def handle_secondary_menu(action, cursor, conexao):
     while True:
@@ -128,16 +117,11 @@ def handle_secondary_menu(action, cursor, conexao):
             break
         elif next_step == 2:
             action(cursor, conexao)
-        elif next_step == 3:
-            print('Encerrando o programa')
-            conexao.close()
-            return True
         else:
-            print('Entrada inválida, por favor digite um número entre 1 e 5')
-    return False
+            print('Entrada inválida, por favor digite 1 para voltar ao menu principal, ou 2 para realizar a mesma tarefa')
 
 def user_decision(cursor, conexao):
-    
+
     while True:
         user_choice = main_menu()
 
@@ -159,7 +143,7 @@ def user_decision(cursor, conexao):
 
         elif user_choice == 5:
             print('Encerrando o programa')
-
+            conexao.close()
             break
         else:
             print('Entrada inválida, por favor digite um número entre 1 e 5')
